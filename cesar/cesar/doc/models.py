6b2997123ea988903f9ff9e6b271c16792f9a01e
ErwinKomen <E.Komen@ru.nl>
2019-03-11 16:44:24 +0100

    Next step in [doc]

--- cesar/cesar/doc/models.py
+++ cesar/cesar/doc/models.py
@@ -82,117 +82,91 @@ class FrogLink(models.Model):
         errHandle = ErrHandle()
         oDoc = None
         iCount = 0
+        inputType = "folia"
+
+        folProc = FoliaProcessor(username)
         try:
             # Directory: one directory for each user
-            dir = os.path.abspath(os.path.join( WRITABLE_DIR, username))
-            if not os.path.exists(dir):
-                os.mkdir(dir)
+            dir = folProc.dir
 
-            # Read the data into a JSON object -- then each 'line' is one 'paragraph'
-            oJson = import_data_file(data_file, errHandle)
-
-            # Action depends on the [froglocation]
-            errHandle.Status("froglocation={}".format(froglocation))
-
-            if froglocation == "local":
-                # We can make use of the local frog
-                # frog = Frog(FrogOptions(parser=False, xmlout=True))
-                frog = Frog(FrogOptions(parser=False))
-
-                # create a folia document with a numbered id
-                docstr = filename
-                doc = folia.Document(id=docstr)
-
-                # Walk through the JSON 
-                lFolia = []
-                for sLine in oJson:
-                    # Append a paragraph
-                    para = doc.append(folia.Paragraph)
-                    # This produces one chunk of FoLiA
-                    parsed_output = frog.process(sLine)
-                    ## The output is an instance of [folia.Document]
-                    #lFolia.append(output)
-                    # The output is a list of dictionary objects
-                    errHandle.Status(json.dumps(parsed_output))
-                # TODO: combine lFolia into one oDoc
-                # oDoc = None
+            # Read and create basis-folia
+            if not folProc.basis_folia(filename, data_file):
+                # There was some kind of error
+                oBack['status'] = 'error'
+                oBack['msg'] = "Could not create BasisFolia"
             else:
-                # create a folia document with a numbered id
-                docstr = filename
-                doc = folia.Document(id=docstr)
-                text = doc.add(folia.Text)
+                # DEBUG: show the frog location
+                errHandle.Status("DEBUG: froglocation={}".format(froglocation))
 
-                # Walk through the JSON 
-                lFolia = []
-                for sLine in oJson:
-                    # Check for empty
-                    sLine = sLine.strip()
-                    if sLine != "":
-                        # Append a paragraph
-                        para = text.add(folia.Paragraph)
-                        # Set the <t> of this paragraph
-                        para.settext(sLine)
-                        # Split text into sentences
-                        sf = StringIO(sLine)
-                        for s_list in pynlpl.textprocessors.Tokenizer(sf, splitsentences=True):
-                            for s in s_list:
-                                sentence = para.add(folia.Sentence)
-                                for token in s:
-                                    sentence.add(folia.Word, token)
+                # Action depends on the [froglocation]
+                if froglocation == "local":
+                    # We can make use of the locally available frog
+                    frog = Frog(FrogOptions(parser=False))
 
-                # THink of a correct name for Basic folia
-                f =  os.path.abspath(os.path.join(dir, filename) + ".xml")
-                # Save the doc
-                doc.save(f)
-
-                    
-                # Think of a project name
-                project = self.name
-                basicauth = True
-                # Get access to the webservice
-                clamclient = CLAMClient(frogurl, clamuser, clampw, basicauth = basicauth)
-                result = clamclient.create(project)
-                data = clamclient.get(project)
-                it = data.inputtemplate('maininput')
-                for param in it.parameters:
-                    if param.id == 'docid':
-                        param.value = self.name
-                        param.hasvalue = True
-                    elif param.id == 'author':
-                        param.value = "cesar"
-                        param.hasvalue = True
-                it.filename = self.name
+                    doc = folProc.doc
+                    # Iterate over all the sentences
+                    for sentence in doc.sentences():
+                        # Get the text of this sentence
+                        sLine = sentence.text()
+                        # This produces one chunk of FoLiA
+                        parsed_output = frog.process(sLine)
+                        ## The output is an instance of [folia.Document]
+                        #lFolia.append(output)
+                        # The output is a list of dictionary objects
+                        errHandle.Status(json.dumps(parsed_output))
+                    # TODO: combine lFolia into one oDoc
+                    # oDoc = None
+                else:
+                    # Think of a project name
+                    project = folProc.docstr
+                    basicauth = True
+                    # Get access to the webservice
+                    clamclient = CLAMClient(frogurl, clamuser, clampw, basicauth = basicauth)
+                    result = clamclient.create(project)
+                    data = clamclient.get(project)
+                    # If we have textinput, then it is 'maininput' (see /frog/info)
+                    if inputType == "text":
+                        it = data.inputtemplate('maininput')
+                        for param in it.parameters:
+                            if param.id == 'author':
+                                param.value = "cesar"
+                                param.hasvalue = True
+                        it.filename = self.name
+                    else:
+                        it = data.inputtemplate("foliainput")
                 
-                # Add the file as input to the project
-                f = os.path.abspath(os.path.join(dir, filename))
-                with open(f, "wb") as destination:
-                    destination.write(data_file.read())
-                # f = "./probeersel.txt"
-                result = clamclient.addinputfile(project, it, f, language='nl')
-                # Opstarten
-                result = clamclient.start(project)
-                if result.errors:
-                    # Handle errors
-                    sys.exit(1)
-                # Otherwise loop until ready
-                while result.status != clam.common.status.DONE:
-                    time.sleep(1)			            # Wacht 4 seconden
-                    result = clamclient.get(project)	# Refresh status
-                    statusmsg = result.statusmessage
-                    completion = result.completion
-                    if oStatus != None:
-                        msg = "{}: {}% completed".format(statusmsg, completion)
-                        oStatus.set("CLAM", msg=msg)
-                # Now we are ready
-                for outputfile in result.output:
-                    name = str(outputfile)
-                    if ".xml"  in name or ".frog.out" in name:
-                        # Download the Folia XML file to (current dir)
-                        fout = os.path.abspath(os.path.join(dir, os.path.basename(str(outputfile))))
-                        outputfile.copy(fout)
-                        iCount += 1
-                # Delete project again
-                clamclient.delete(project)
+                    # Add the Basis-Folia XML file as input to the project
+                    basicf = folProc.basicf
+                    result = clamclient.addinputfile(project, it, basicf, language='nl')
+                    # Opstarten
+                    result = clamclient.start(project)
+                    if result.errors:
+                        # Handle errors
+                        sys.exit(1)
+                    # Otherwise loop until ready
+                    while result.status != clam.common.status.DONE:
+                        time.sleep(1)			            # Wacht 4 seconden
+                        result = clamclient.get(project)	# Refresh status
+                        statusmsg = result.statusmessage
+                        completion = result.completion
+                        if oStatus != None:
+                            msg = "{}: {}% completed".format(statusmsg, completion)
+                            oStatus.set("CLAM", msg=msg)
+                    # Now we are ready
+                    for outputfile in result.output:
+                        name = str(outputfile)
+                        if ".xml"  in name or ".frog.out" in name:
+                            # Download the Folia XML file to (current dir)
+                            fout = os.path.abspath(os.path.join(dir, os.path.basename(str(outputfile))))
+                            outputfile.copy(fout)
+                            iCount += 1
+                        else:
+                            # Download the Folia XML file to (current dir)
+                            fout = os.path.abspath(os.path.join(dir, os.path.basename(str(outputfile))))
+                            outputfile.copy(fout)
+                            iCount += 1
+                    # Delete project again
+                    clamclient.delete(project)
   
 
 
@@ -209,4 +183,75 @@ class FrogLink(models.Model):
         # Return the object that has been created
         return oBack
 
-    
+class FoliaProcessor():
+    """Functions to help create or process a folia document"""
+
+    docstr = ""         # The identifier of this document
+    username = ""       # The owner of this document
+    dir = ""            # Directory for the output
+    basicf = ""         # If any: where the basis folis is
+    doc = None
+
+    def __init__(self, username):
+        # Check and/or create the appropriate directory for the user
+        dir = os.path.abspath(os.path.join( WRITABLE_DIR, username))
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        # Set the dir locally
+        self.dir = dir
+
+    def basis_folia(self, filename, data_contents):
+
+        errHandle = ErrHandle()
+        bReturn = False
+        try:
+            # Read file into array
+            lines = []
+            for line in data_contents:
+                lines.append(line.decode("utf-8").strip())
+
+            # Check and/or create the appropriate directory for the user
+            dir = self.dir
+
+            # create a folia document with a numbered id
+            docstr = os.path.splitext( os.path.basename(filename))[0]
+            # Make sure we remember the docstr
+            self.docstr = docstr
+
+            # Start creating a folia document
+            doc = folia.Document(id=docstr)
+            text = doc.add(folia.Text)
+
+            # Walk through the JSON 
+            lFolia = []
+            for sLine in lines:
+                # Check for empty
+                sLine = sLine.strip()
+                if sLine != "":
+                    # Append a paragraph
+                    para = text.add(folia.Paragraph)
+                    # Set the <t> of this paragraph
+                    para.settext(sLine)
+                    # Split text into sentences
+                    sf = StringIO(sLine)
+                    for s_list in pynlpl.textprocessors.Tokenizer(sf, splitsentences=True):
+                        for s in s_list:
+                            sentence = para.add(folia.Sentence)
+                            for token in s:
+                                sentence.add(folia.Word, token)
+
+            # THink of a correct name for Basic folia
+            f =  os.path.abspath(os.path.join(dir, docstr) + ".basis.xml")
+            self.basicf = f
+            # Save the doc
+            doc.save(f)
+            # Make sure the doc is in this class instance
+            self.doc = doc
+            # Set positive return
+            bReturn = True
+        except:
+            sMsg = errHandle.get_error_message()
+            errHandle.DoError("basis_folia")
+            bReturn = False
+        # Return what has happened
+        return bReturn
