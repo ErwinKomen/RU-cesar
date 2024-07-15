60f660c6222b0f70bd059765cc1a50dbd653a17e
Erwin Komen <Erwin_Komen@SIL.org>
2024-07-15 11:49:55 +0200

    tsg handle update

--- cesar/cesar/tsg/models.py
+++ cesar/cesar/tsg/models.py
@@ -1,9 +1,23 @@
+"""Models for the TSG app.
+
+TSG is Radboud Humanities lab's technical service group.
+Some of the models here serve other parts of the CESAR application.
+The PidService serves the 'tsg' app within Cesar, that is for use by the TSG.
+
+"""
 import json
 from django.db import models
 from django.utils import timezone
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+# ========= own application
 from cesar.utils import ErrHandle
 
 MAXPARAMLEN = 100
+MAX_NAME_LEN = 50
+MAX_STRING_LEN = 255
 
 def get_crpp_date(dtThis):
     """Convert datetime to string"""
@@ -11,6 +25,10 @@ def get_crpp_date(dtThis):
     # Model: yyyy-MM-dd'T'HH:mm:ss
     sDate = dtThis.strftime("%Y-%m-%dT%H:%M:%S")
     return sDate
+
+
+# =================================== TSG MODELS ===================================================
+
 
 class TsgInfo(models.Model):
     """Each instance contains a bit of information that can be used by the TSG"""
@@ -48,7 +66,7 @@ class TsgHandle(models.Model):
     # [1] The handle code
     code = models.CharField("Handle code", max_length=MAXPARAMLEN)
     # [1] The current URL this handle links to
-    url = models.URLField("Handle URL")
+    url = models.URLField("Handle URL", blank=True, null=True)
     # [1] The handle domain
     domain = models.CharField("Handle base url", max_length=MAXPARAMLEN, default="21.11114")
     # [1] Information on this handle in stringified JSON
@@ -59,15 +77,24 @@ class TsgHandle(models.Model):
     history = models.TextField("History", default = "[]")
     # [0-1] Notes on this particular handle (if needed)
     notes = models.TextField("Notes", blank=True, null=True)
+    # [1] Status of this handle: ini, set, chg
+    status = models.CharField("Status", max_length= MAXPARAMLEN, default="ini")
 
     def __str__(self):
         return self.code
 
-    def full_handle(self):
+    def full_handle(self, plain=True):
         """Provide the full url of the handle, based on the current code"""
 
-        url = "http://hdl.handle.net/{}/{}".format(self.domain, self.code)
-        return url
+        sBack = ""
+        if self.domain != "" and self.code != "":
+            url = "https://hdl.handle.net/{}/{}".format(self.domain, self.code)
+            if plain:
+                sBack = url
+            else:
+                # Pack it into something clickable
+                sBack = '<span class="badge signature gr"><a href="{}">{}</a></span>'.format(url, url)
+        return sBack
 
     def add_handle(code, url, domain="21.11114", info=None):
         oErr = ErrHandle()
@@ -89,66 +116,104 @@ class TsgHandle(models.Model):
         obj = TsgHandle.objects.filter(code=code).first()
         return (obj != None)
 
-    def get_info(self, section = None):
-        oResponse = {}
-        method = "new"
-        if section == None:
-            oResponse['section'] = 'plain'
-            oResponse['data'] = json.loads(self.info)
-        else:
-            lst_info = json.loads(self.info)
-            section = section.lower()
-            for obj_info in lst_info:
-                if obj_info['type'].lower() == section:
-                    if method == "old":
-                        # We have the right section
-                        if section == "url" or section == "inst":
-                            response = "{}, {}, privs={}: {}".format(
-                                 obj_info['idx'], obj_info['timestamp'], obj_info['privs'], obj_info['parsed_data'])
-                        elif section == "hs_admin":
-                            # Process the parsed data
-                            parsed = obj_info['parsed_data']
-                            lData = []
-                            for k,v in parsed.items():
-                                if k == "perms": 
-                                    for k2, v2 in v.items():
-                                        lData.append("{}={}".format(k2,v2))
-                                else:
-                                    lData.append("{}={}".format(k,v))
-                            sData = ", ".join(lData)
-                            response = "{}, idx {}, privs={}: {}".format(
-                                 obj_info['timestamp'], obj_info['idx'], obj_info['privs'], sData)
-                    else:
-                        # Get basic information
-                        oResponse['section'] = section
-                        oResponse['idx'] = obj_info['idx']
-                        oResponse['privs'] = obj_info['privs']
-                        oResponse['timestamp'] = obj_info['timestamp']
-                        if section == "url" or section == "inst":
-                            oResponse['data'] = obj_info['parsed_data']
-                        elif section == "hs_admin":
-                            # Process the parsed data
-                            parsed = obj_info['parsed_data']
-                            lData = []
-                            for k,v in parsed.items():
-                                if k == "perms": 
-                                    for k2, v2 in v.items():
-                                        lData.append("{}={}".format(k2,v2))
-                                else:
-                                    lData.append("{}={}".format(k,v))
-                            sData = ", ".join(lData)
-                            oResponse['data'] = sData
+    def get_created(self):
+        sBack = self.created.strftime("%d/%b/%Y %H:%M")
+        return sBack
 
-                    # Now break away
-                    break
+    def get_history(self, plain=True):
+        sBack = "-"
+        oErr = ErrHandle()
+        try:
+            if plain:
+                sBack = json.loads(self.history)
+            else:
+                lHistory = json.loads(self.history)
+                if len(lHistory) > 0:
+                    # Create a table myself
+                    html = []
+                    html.append("<table><thead><tr><th>#</th><th>occasion</th></tr></thead><tbody>")
+                    for idx, hist_item in enumerate(lHistory):
+                        html.append("<tr><td>{}</td><td>{}</td></tr>".format(idx+1, hist_item))
+                    html.append("</tbody></table>")
+                    sBack = "\n".join(html)
+        except:
+            msg = oErr.get_error_message()
+            oErr.DoError("tsghandle/get_history")
+
+        return sBack
+
+    def get_info(self, section = None):
+        oResponse = None
+        method = "new"
+        oErr = ErrHandle()
+        try:
+            if section == None:
+                oResponse = dict(section="plain", data=json.loads(self.info))
+            else:
+                lst_info = json.loads(self.info)
+                section = section.lower()
+                for obj_info in lst_info:
+                    if obj_info['type'].lower() == section:
+                        if method == "old":
+                            # We have the right section
+                            if section == "url" or section == "inst":
+                                response = "{}, {}, privs={}: {}".format(
+                                     obj_info['idx'], obj_info['timestamp'], obj_info['privs'], obj_info['parsed_data'])
+                            elif section == "hs_admin":
+                                # Process the parsed data
+                                parsed = obj_info['parsed_data']
+                                lData = []
+                                for k,v in parsed.items():
+                                    if k == "perms": 
+                                        for k2, v2 in v.items():
+                                            lData.append("{}={}".format(k2,v2))
+                                    else:
+                                        lData.append("{}={}".format(k,v))
+                                sData = ", ".join(lData)
+                                response = "{}, idx {}, privs={}: {}".format(
+                                     obj_info['timestamp'], obj_info['idx'], obj_info['privs'], sData)
+                        else:
+                            # Get basic information
+                            oResponse = dict(section=section)
+                            oResponse['idx'] = obj_info['idx']
+                            oResponse['privs'] = obj_info['privs']
+                            oResponse['timestamp'] = obj_info['timestamp']
+                            if section == "url" or section == "inst":
+                                oResponse['data'] = obj_info['parsed_data']
+                            elif section == "hs_admin":
+                                # Process the parsed data
+                                parsed = obj_info['parsed_data']
+                                lData = []
+                                for k,v in parsed.items():
+                                    if k == "perms": 
+                                        for k2, v2 in v.items():
+                                            lData.append("{}={}".format(k2,v2))
+                                    else:
+                                        lData.append("{}={}".format(k,v))
+                                sData = ", ".join(lData)
+                                oResponse['data'] = sData
+
+                        # Now break away
+                        break
+        except:
+            msg = oErr.get_error_message()
+            oErr.DoError("tsghandle/get_info")
         return oResponse
 
     def get_info_all(self):
         """Get the information from all three sections"""
+
         sections = ["URL", "INST", "HS_ADMIN"]
         lResponse = []
-        for section in sections:
-            lResponse.append(self.get_info(section))
+        oErr = ErrHandle()
+        try:
+            for section in sections:
+                oInfo = self.get_info(section)
+                if not oInfo is None:
+                    lResponse.append(oInfo)
+        except:
+            msg = oErr.get_error_message()
+            oErr.DoError("tsghandle/get_info_all")
         return lResponse
 
     def get_info_url(self):
@@ -163,5 +228,51 @@ class TsgHandle(models.Model):
         """Get the [HS_ADMIN] section of the information"""
         return self.get_info("HS_ADMIN")
 
-    def get_history(self):
-        return json.loads(self.history)
+    def get_info_html(self):
+        """Get information in the form of HTML code"""
+
+        sBack = "-"
+        oErr = ErrHandle()
+        try:
+            lst_info = self.get_info_all()
+            if len(lst_info) > 0:
+                # Create a table myself
+                html = []
+                html.append('<table class="func-view"><thead><tr><th>Section</th><th>idx</th><th>Timestamp</th>')
+                html.append('<th>privs</th><th>Value</th></tr></thead><tbody>')
+                for idx, info_item in enumerate(lst_info):
+                    html.append('<tr>')
+                    html.append(' <td valign="top">{}</td>'.format(info_item.get('section')))
+                    html.append(' <td valign="top">{}</td>'.format(info_item.get('idx')))
+                    html.append(' <td valign="top">{}</td>'.format(info_item.get('timestamp')))
+                    html.append(' <td valign="top">{}</td>'.format(info_item.get('privs')))
+                    html.append(' <td valign="top">{}</td>'.format(info_item.get('data')))
+                    html.append('</tr>')
+                html.append('</tbody></table>')
+                sBack = "\n".join(html)
+
+        except:
+            msg = oErr.get_error_message()
+            oErr.DoError("tsghandle/get_info_html")
+
+        return sBack
+
+    def get_status(self):
+        sBack = ""
+        status = self.status
+        if not status is None and self.status != "":
+            oStatusDict = dict(ini="Initialized", set="Handle set", chg="URL changed")
+            sBack = oStatusDict.get(status, "???")
+        return sBack
+
+    def get_url(self, plain=True):
+        """Show the URL this handle defines"""
+
+        sBack = ""
+        if not self.url is None:
+            if plain:
+                sBack = self.url
+            else:
+                sBack = '<span class="badge signature cl"><a href="{}">{}</a></span>'.format(self.url, self.url)
+        return sBack
+
